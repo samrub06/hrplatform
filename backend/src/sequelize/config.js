9c790be1a85fae4494f02560b562a9046114a970
Samuel Charbit <76777361+samrub06@users.noreply.github.com>
2025-05-30 11:05:38 +0300

    connexionpostgress

--- backend/src/sequelize/config.js
+++ backend/src/sequelize/config.js
@@ -20,11 +20,23 @@ const config = {
   },
   production: {
     dialect: 'postgres',
-    username: process.env.DB_USER || '',
-    password: process.env.DB_PASSWORD || '',
-    host: process.env.DB_HOST || '',
-    port: parseInt(process.env.DB_PORT || '5432', 10),
-    database: process.env.DB_NAME || '',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT, 10),
+    database: process.env.DB_NAME,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
   },
 };
 
