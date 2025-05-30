c084c3ff2cce279685b522f040356d339e0c9dba
Samuel Charbit <76777361+samrub06@users.noreply.github.com>
2025-05-30 12:03:15 +0300

    public routes

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
+import { Public } from './casl/public.decorator';
 
 @Controller()
 export class AppController {
+  @Public()
   @Get()
   redirectToSwagger(@Res() res: Response) {
     return res.redirect('/swagger');
