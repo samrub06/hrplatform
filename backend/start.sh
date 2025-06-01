cdf8d377708454bf2634ca0d6ed1626705ef2b15
Samuel Charbit <76777361+samrub06@users.noreply.github.com>
2025-06-01 15:31:56 +0300

    fix redirection

--- backend/start.sh
+++ backend/start.sh
@@ -4,7 +4,7 @@ echo "En attente de la base de données..."
 sleep 10
 
 echo "Vérification des migrations..."
-npx sequelize-cli db:migrate
+cd /app && npx sequelize-cli db:migrate
 
 echo "Démarrage de l'application..."
 npm run start:prod 
