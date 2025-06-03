e8766bdcb4459a84d7d19eabc5efc206c2eeaab8
Samuel Charbit <76777361+samrub06@users.noreply.github.com>
2025-06-03 16:30:55 +0300

    ok

--- backend/src/sequelize/seeders/20250110000000-init-roles-permissions.js
+++ backend/src/sequelize/seeders/20250110000000-init-roles-permissions.js
@@ -7,74 +7,74 @@ module.exports = {
       // Création des permissions
       const permissions = [
         // Permissions pour le domaine User
-        { 
-          id: uuidv4(), 
-          domain: 'User', 
-          name: "ReadUser", 
-          can_create: false, 
-          can_read: true, 
-          can_edit: false, 
-          can_delete: false, 
-          createdAt: new Date(), 
-          updatedAt: new Date() 
+        {
+          id: uuidv4(),
+          domain: 'User',
+          name: 'ReadUser',
+          can_create: false,
+          can_read: true,
+          can_edit: false,
+          can_delete: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
-        { 
-          id: uuidv4(), 
-          domain: 'User', 
-          name: "EditUser", 
-          can_create: false, 
-          can_read: true, 
-          can_edit: true, 
-          can_delete: false, 
-          createdAt: new Date(), 
-          updatedAt: new Date() 
+        {
+          id: uuidv4(),
+          domain: 'User',
+          name: 'EditUser',
+          can_create: false,
+          can_read: true,
+          can_edit: true,
+          can_delete: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
-        { 
-          id: uuidv4(), 
-          domain: 'User', 
-          name: "FullAccessUser", 
-          can_create: true, 
-          can_read: true, 
-          can_edit: true, 
-          can_delete: true, 
-          createdAt: new Date(), 
-          updatedAt: new Date() 
+        {
+          id: uuidv4(),
+          domain: 'User',
+          name: 'FullAccessUser',
+          can_create: true,
+          can_read: true,
+          can_edit: true,
+          can_delete: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
-        
+
         // Permissions pour le domaine Job
-        { 
-          id: uuidv4(), 
-          domain: 'Job', 
-          name: "ReadJob", 
-          can_create: false, 
-          can_read: true, 
-          can_edit: false, 
-          can_delete: false, 
-          createdAt: new Date(), 
-          updatedAt: new Date() 
+        {
+          id: uuidv4(),
+          domain: 'Job',
+          name: 'ReadJob',
+          can_create: false,
+          can_read: true,
+          can_edit: false,
+          can_delete: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
-        { 
-          id: uuidv4(), 
-          domain: 'Job', 
-          name: "CreateJob", 
-          can_create: true, 
-          can_read: true, 
-          can_edit: false, 
-          can_delete: false, 
-          createdAt: new Date(), 
-          updatedAt: new Date() 
+        {
+          id: uuidv4(),
+          domain: 'Job',
+          name: 'CreateJob',
+          can_create: true,
+          can_read: true,
+          can_edit: false,
+          can_delete: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
         },
-        { 
-          id: uuidv4(), 
-          domain: 'Job', 
-          name: "FullAccessJob", 
-          can_create: true, 
-          can_read: true, 
-          can_edit: true, 
-          can_delete: true, 
-          createdAt: new Date(), 
-          updatedAt: new Date() 
-        }
+        {
+          id: uuidv4(),
+          domain: 'Job',
+          name: 'FullAccessJob',
+          can_create: true,
+          can_read: true,
+          can_edit: true,
+          can_delete: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
       ];
 
       await queryInterface.bulkInsert('permission', permissions);
@@ -82,9 +82,24 @@ module.exports = {
 
       // Création des rôles
       const roles = [
-        { id: uuidv4(), name: 'candidate', createdAt: new Date(), updatedAt: new Date() },
-        { id: uuidv4(), name: 'publisher', createdAt: new Date(), updatedAt: new Date() },
-        { id: uuidv4(), name: 'admin', createdAt: new Date(), updatedAt: new Date() }
+        {
+          id: uuidv4(),
+          name: 'candidate',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: uuidv4(),
+          name: 'publisher',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: uuidv4(),
+          name: 'admin',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
       ];
 
       await queryInterface.bulkInsert('role', roles);
@@ -92,8 +107,10 @@ module.exports = {
 
       // Récupération des IDs des permissions et rôles insérés
       const [savedPermissions, savedRoles] = await Promise.all([
-        queryInterface.sequelize.query('SELECT id, name, domain FROM permission'),
-        queryInterface.sequelize.query('SELECT id, name FROM role')
+        queryInterface.sequelize.query(
+          'SELECT id, name, domain FROM permission',
+        ),
+        queryInterface.sequelize.query('SELECT id, name FROM role'),
       ]);
 
       // Création de maps pour faciliter l'accès aux IDs
@@ -115,54 +132,54 @@ module.exports = {
           role_id: rolesMap.candidate,
           permission_id: permissionsMap.User_EditUser,
           createdAt: new Date(),
-          updatedAt: new Date()
+          updatedAt: new Date(),
         },
         {
           id: uuidv4(),
           role_id: rolesMap.candidate,
           permission_id: permissionsMap.Job_ReadJob,
           createdAt: new Date(),
-          updatedAt: new Date()
+          updatedAt: new Date(),
         },
-        
+
         // Permissions pour le rôle publisher
         {
           id: uuidv4(),
           role_id: rolesMap.publisher,
           permission_id: permissionsMap.User_ReadUser,
           createdAt: new Date(),
-          updatedAt: new Date()
+          updatedAt: new Date(),
         },
         {
           id: uuidv4(),
           role_id: rolesMap.publisher,
           permission_id: permissionsMap.Job_CreateJob,
           createdAt: new Date(),
-          updatedAt: new Date()
+          updatedAt: new Date(),
         },
         {
           id: uuidv4(),
           role_id: rolesMap.publisher,
           permission_id: permissionsMap.Job_FullAccessJob,
           createdAt: new Date(),
-          updatedAt: new Date()
+          updatedAt: new Date(),
         },
-        
+
         // Permissions pour le rôle admin (accès complet)
         {
           id: uuidv4(),
           role_id: rolesMap.admin,
           permission_id: permissionsMap.User_FullAccessUser,
           createdAt: new Date(),
-          updatedAt: new Date()
+          updatedAt: new Date(),
         },
         {
           id: uuidv4(),
           role_id: rolesMap.admin,
           permission_id: permissionsMap.Job_FullAccessJob,
           createdAt: new Date(),
-          updatedAt: new Date()
-        }
+          updatedAt: new Date(),
+        },
       ];
 
       await queryInterface.bulkInsert('role_permission', rolePermissions);
@@ -180,5 +197,5 @@ module.exports = {
     await queryInterface.bulkDelete('role_permission', null, {});
     await queryInterface.bulkDelete('permission', null, {});
     await queryInterface.bulkDelete('role', null, {});
-  }
-}; 
+  },
+};
