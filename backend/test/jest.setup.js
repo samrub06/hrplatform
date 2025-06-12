14a953c9f2567f2496d3330f339996cc673cf3d8
Samuel Charbit <76777361+samrub06@users.noreply.github.com>
2025-06-12 19:35:05 +0300

    fix/update-jest

---
+++ backend/test/jest.setup.js
@@ -0,0 +1,21 @@
+global.React = require('react');
+
+// Mock les composants @react-email
+jest.mock('@react-email/components', () => ({
+  Html: ({ children }) => children,
+  Head: ({ children }) => children,
+  Body: ({ children }) => children,
+  Container: ({ children }) => children,
+  Text: ({ children }) => children,
+  Link: ({ children }) => children,
+  Preview: ({ children }) => children,
+  Section: ({ children }) => children,
+  Column: ({ children }) => children,
+  Row: ({ children }) => children,
+  Button: ({ children }) => children,
+  Img: ({ src, alt }) => `<img src="${src}" alt="${alt}" />`
+}));
+
+jest.mock('@react-email/render', () => ({
+  renderAsync: jest.fn().mockResolvedValue('<div>Mocked Email</div>')
+})); 
